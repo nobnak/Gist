54d1e2c7cf3d597bbe27348a6f7af6cdd755cb9c
Nobuyuki Nakata <nobu.nakata@gmail.com>
2016-10-26 15:37:15 +0900

    拡張メソッドを幾つか追加 インデックス付きリスト

---
+++ Extensions/AABBExtension.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Gist.Extensions.AABB {
+
+    public static class AABBExtension {
+        #region Extensions
+        public static Bounds Encapsulate(this IEnumerable<Bounds> bounds) {
+            var resmin = Min ();
+            var resmax = Max ();
+            foreach (var bb in bounds) {
+                var bbmin = bb.min;
+                var bbmax = bb.max;
+                for (var i = 0; i < 3; i++) {
+                    resmin [i] = Mathf.Min (resmin [i], bbmin [i]);
+                    resmax [i] = Mathf.Max (resmax [i], bbmax [i]);
+                }
+            }
+            return MinMaxBounds (resmin, resmax);
+        }
+        public static Bounds Encapsulate(this IEnumerable<Vector3> poss) {
+            var resmin = Min ();
+            var resmax = Max ();
+            foreach (var p in poss) {
+                for (var i = 0; i < 3; i++) {
+                    resmin [i] = Mathf.Min (resmin [i], p[i]);
+                    resmax [i] = Mathf.Max (resmax [i], p[i]);
+                }
+            }
+            return MinMaxBounds (resmin, resmax);
+        }
+        #endregion
+
+        public static Bounds MinMaxBounds(Vector3 min, Vector3 max) {
+            var bb = new Bounds();
+            bb.SetMinMax(min, max);
+            return bb;
+        }
+        public static Vector3 Min() {
+            return new Vector3 (float.MaxValue, float.MaxValue, float.MaxValue);
+        }
+        public static Vector3 Max() {
+            return new Vector3 (float.MinValue, float.MinValue, float.MinValue);
+        }
+    }
+}
---
+++ Extensions/RangeExtension.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace Gist.Extensions.Range {
+
+    public static class RangeExtension {
+    
+        public static IEnumerable<T> Range<T>(this IEnumerable<T> seq, int offset, int length) {
+            var iter = seq.GetEnumerator ();
+
+            for (var i = 0; iter.MoveNext () && i < offset; i++)
+                ;
+            for (var i = 0; iter.MoveNext () && i < length; i++)
+                yield return iter.Current;
+        }
+        public static IEnumerable<T> Range<T>(this IList<T> seq, int offset, int length) {
+            for (var i = 0; i < length; i++)
+                yield return seq [offset + i];
+        }
+        public static IEnumerable<T> Range<T>(this IReadOnlyList<T> seq, int offset, int length) {
+            for (var i = 0; i < length; i++)
+                yield return seq[offset + i];
+        }
+    }
+}
