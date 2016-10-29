a740394c97489c3b37f57ecede25a410fc91e448
Naka Nobuyuki <nobu.nakata@gmail.com>
2016-10-29 12:44:23 +0900

    Easing 追加

---
+++ Extensions/EasingExtension.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Gist.Extensions.Easing {
+
+	public static class EasingExtension {
+
+		public static Easer Easing(this MonoBehaviour bhv, System.Action<float> func) {
+			
+		}
+	}
+
+	public class Easer {
+		MonoBehaviour _bhv;
+		System.Action<float> _func;
+
+		public Easer(Component comp, System.Action<float> func) {
+			
+		}
+	}
+}
