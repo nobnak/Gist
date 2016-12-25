7327b7e1f74a50393735c9af7e2833fc3e0c7947
Nobuyuki Nakata <nobu.nakata@gmail.com>
2016-12-25 18:07:04 +0900

    シリアライザを MonoBehaviour カラ分離

--- Settings.cs
+++ Settings.cs
@@ -6,18 +6,60 @@ using DataUI;
 namespace Gist {
         
     public abstract class Settings : MonoBehaviour {
-        public const float WINDOW_WIDTH = 300f;
         public SettingsCore core;
-        public enum ModeEnum { Normal = 0, GUI = 1, End = 2 }
-        public ModeEnum mode;
-        public KeyCode toggleKey = KeyCode.None;
+
 
         [System.Serializable]
         public class SettingsCore {
+            public const float WINDOW_WIDTH = 300f;
+            public enum ModeEnum { Normal = 0, GUI = 1, End = 2 }
+
+            public UnityEngine.Events.UnityEvent OnDataChange;
+
+            public ModeEnum mode;
+            public KeyCode toggleKey = KeyCode.None;
+
             public enum PathTypeEnum { StreamingAssets = 0, MyDocuments }
 
             public PathTypeEnum pathType;
             public string dataPath;
+
+            #region Behaviour Wrapper
+            FieldEditor _dataEditor;
+            Rect _window;
+
+            public virtual void OnEnable<T>(T data) {
+                Load (data);
+                _dataEditor = new FieldEditor (data);
+                NotifyOnDataChange ();
+            }
+            public virtual void Update<T>(T data) {
+                if (Input.GetKeyDown (toggleKey)) {
+                    mode = (ModeEnum)(((int)mode + 1) % (int)ModeEnum.End);
+                    if (mode == ModeEnum.Normal)
+                        Save (data);
+                }
+
+                switch (mode) {
+                case ModeEnum.GUI:
+                    NotifyOnDataChange ();
+                    break;
+                }
+            }
+            public virtual void OnGUI(MonoBehaviour b) {
+                if (mode == ModeEnum.GUI)
+                    _window = GUILayout.Window (b.GetInstanceID(), _window, Window, b.name, GUILayout.MinWidth (WINDOW_WIDTH));
+            }
+
+            void NotifyOnDataChange () {
+                OnDataChange.Invoke ();
+            }
+
+            void Window(int id) {
+                _dataEditor.OnGUI ();
+                GUI.DragWindow ();
+            }
+            #endregion
 
             #region Save/Load
             public virtual void Load<T>(T data) {
@@ -56,41 +98,28 @@ namespace Gist {
         }
     }
     public abstract class Settings<T> : Settings {
+        public event System.Action EventOnDataChange;
+
         public T data;
 
-        FieldEditor _dataEditor;
-        Rect _window;
-
+        protected virtual void Awake() {
+            core.OnDataChange.AddListener (new UnityEngine.Events.UnityAction (NotifyOnDataChange));
+        }
         protected virtual void OnEnable() {
-            core.Load (data);
-            _dataEditor = new FieldEditor (data);
-            OnDataChange ();
+            core.OnEnable (data);
         }
         protected virtual void Update() {
-            if (Input.GetKeyDown (toggleKey)) {
-                mode = (ModeEnum)(((int)mode + 1) % (int)ModeEnum.End);
-                if (mode == ModeEnum.Normal)
-                    core.Save (data);
-            }
-
-            switch (mode) {
-            case ModeEnum.GUI:
-                OnDataChange ();
-                break;
-            }
+            core.Update (data);
         }
         protected virtual void OnGUI() {
-            if (mode == ModeEnum.GUI)
-                _window = GUILayout.Window (GetInstanceID(), _window, Window, this.name, GUILayout.MinWidth (WINDOW_WIDTH));
+            core.OnGUI (this);
         }
 
+        protected virtual void NotifyOnDataChange() {
+            if (EventOnDataChange != null)
+                EventOnDataChange ();
+            OnDataChange ();
+        }
         protected virtual void OnDataChange() {}
-
-        #region GUI
-        void Window(int id) {
-            _dataEditor.OnGUI ();
-            GUI.DragWindow ();
-        }
-        #endregion
     }
 }
