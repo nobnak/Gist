cd0ca2a02ea377266d7e35608fec0d7834302bcb
Nobuyuki Nakata <nobu.nakata@gmail.com>
2017-04-17 21:30:09 +0900

    タイマーが予定時間を経過したかのフラグを追加

--- Timer.cs
+++ Timer.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 namespace Gist { 
 
     public class Timer {
+        public enum StateEnum { Init = 0, Active, Completed }
+
         public event System.Action<Timer> Elapsed;
 
         protected bool active;
+        protected bool completed;
+
         protected float interval;
         protected float timeOfElapsed;
 
         public Timer(float interval) {
+            Goto (StateEnum.Init);
             Interval = interval;
         }
 
@@ -20,29 +25,47 @@ namespace Gist {
             set {
                 interval = value;
                 if (interval < 0f)
-                    active = false;
+                    Goto (StateEnum.Init);
             }
         }
+        public bool Active { get { return active; } }
+        public bool Completed { get { return completed; } }
 
         public void Start() {
             Start (interval);
         }
         public void Start(float interval) {
-            active = true;
+            Goto (StateEnum.Active);
             Interval = interval;
             timeOfElapsed = Time.timeSinceLevelLoad + interval;
         }
         public void Update() {
             var currentTime = Time.timeSinceLevelLoad;
             if (active && timeOfElapsed <= currentTime) {
-                active = false;
+                Goto (StateEnum.Completed);
                 NotifyElapsed ();
             }
         }
         public void Stop() {
-            active = false;
+            Goto (StateEnum.Init);
         }
 
+        void Goto(StateEnum state) {
+            switch (state) {
+            case StateEnum.Init:
+                active = false;
+                completed = false;
+                break;
+            case StateEnum.Active:
+                active = true;
+                completed = false;
+                break;
+            case StateEnum.Completed:
+                active = false;
+                completed = true;
+                break;
+            }
+        }
         void NotifyElapsed () {
             if (Elapsed != null)
                 Elapsed (this);
