c2976ba07769855f4daedf05ff35902d14c79433
Nobuyuki Nakata <nobu.nakata@gmail.com>
2017-04-28 12:33:11 +0900

    レイヤーパネルを追加

---
+++ LayerPanel.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Gist;
+
+namespace Gist {
+        
+    [ExecuteInEditMode]
+    public class LayerPanel : MonoBehaviour {
+        public enum DepthModeEnum { Normalized = 0, Exact }
+
+        public string defaultTextureName = "_MainTex";
+        public Camera targetCam;
+
+        public DepthModeEnum depthMode;
+        [Header("Normalized Depth")]
+    	[Range(0f, 0.99f)]
+    	public float normalizedDepth = 0f;
+        [Header("Exact Depth")]
+        public float depth = 10f;
+
+        public Vector2 size = Vector2.one;
+        public Rect viewport = new Rect (0f, 0f, 1f, 1f);
+
+        MaterialPropertyBlockChanied block;
+
+        #region Unity
+        void OnEnable() {
+            Renderer rend;
+            if ((rend = GetComponent<Renderer> ()) != null)
+                block = new MaterialPropertyBlockChanied (rend);
+        }
+        void Update() {
+            if (targetCam == null)
+                return;
+
+            var min = viewport.min;
+            var max = viewport.max;
+            var center = viewport.center;
+            var depth = Depth ();
+
+            var scale = targetCam.ViewportToWorldPoint (new Vector3 (max.x, max.y, depth))
+                - targetCam.ViewportToWorldPoint (new Vector3 (min.x, min.y, depth));
+    		scale = targetCam.transform.InverseTransformDirection (scale);
+            scale.x /= size.x;
+            scale.y /= size.y;
+            scale.z = 1f;
+
+            transform.position = targetCam.ViewportToWorldPoint (new Vector3 (center.x, center.y, depth));
+            transform.rotation = targetCam.transform.rotation;
+    		transform.localScale = scale;
+        }
+        #endregion
+
+        public virtual void SetTexture(Texture tex) {
+            SetTexture(defaultTextureName, tex);
+        }
+        public virtual void SetTexture(string textureName, Texture tex) {
+            if (block != null)
+                block.SetTexture (textureName, tex).Apply ();
+        }
+
+        float Depth() {
+            switch (depthMode) {
+            case DepthModeEnum.Normalized:
+                return Mathf.Lerp (targetCam.nearClipPlane, targetCam.farClipPlane, normalizedDepth);
+            default:
+                return depth;
+            }
+        }
+    }
+}
