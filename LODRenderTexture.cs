23ec2d39883f81dccc765af1bb130bf1f959563a
Naka Nobuyuki <nobu.nakata@gmail.com>
2017-04-30 18:00:10 +0900

    アンチエイリアシング対応

--- LODRenderTexture.cs
+++ LODRenderTexture.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 namespace Gist {
     public class LODRenderTexture : System.IDisposable {
+        public const int DEFAULT_ANTIALIASING = -1;
         public delegate void TargetSize(out int width, out int height);
-
         public event System.Action<LODRenderTexture> AfterCreateTexture;
 
         int lod;
@@ -14,26 +14,30 @@ namespace Gist {
         RenderTextureFormat format;
         RenderTexture tex;
         TargetSize size;
+        int antiAliasing;
 
         public LODRenderTexture(TargetSize size, int lod, int depth, RenderTextureFormat format,
-            RenderTextureReadWrite readWrite) {
+            RenderTextureReadWrite readWrite, int antiAliasing) {
             this.size = size;
             this.lod = Mathf.Max(0, lod);
             this.depth = depth;
             this.size = size;
             this.format = format;
             this.readWrite = readWrite;
+            this.antiAliasing = ParseAntiAliasing (antiAliasing);
         }
         public LODRenderTexture(Camera cam, int lod = 0, int depth = 24, 
-            RenderTextureFormat format = RenderTextureFormat.ARGB32,
-            RenderTextureReadWrite readWrite = RenderTextureReadWrite.Default)
+            RenderTextureFormat format = RenderTextureFormat.ARGB32, 
+            RenderTextureReadWrite readWrite = RenderTextureReadWrite.Default,
+            int antiAliasing = DEFAULT_ANTIALIASING)
             : this((out int w, out int h) => { w = cam.pixelWidth; h = cam.pixelHeight; }, 
-                lod, depth, format, readWrite) { }
+                lod, depth, format, readWrite, antiAliasing) { }
         public LODRenderTexture(Texture tex, int lod = 0, int depth = 24, 
             RenderTextureFormat format = RenderTextureFormat.ARGB32,
-            RenderTextureReadWrite readWrite = RenderTextureReadWrite.Default)
+            RenderTextureReadWrite readWrite = RenderTextureReadWrite.Default,
+            int antiAliasing = DEFAULT_ANTIALIASING)
             : this((out int w, out int h) => { w = tex.width; h = tex.height; }, 
-                lod, depth, format, readWrite) { }
+                lod, depth, format, readWrite, antiAliasing) { }
 
         public RenderTexture Texture { get { return tex; } }
         public FilterMode FilterMode { get; set; }
@@ -58,6 +62,7 @@ namespace Gist {
                 tex = new RenderTexture (width, height, depth, format, readWrite);
                 tex.filterMode = FilterMode;
                 tex.wrapMode = WrapMode;
+                tex.antiAliasing = antiAliasing;
                 NotifyAfterCreateTexture ();
                 return true;
             }
@@ -66,6 +71,9 @@ namespace Gist {
         void NotifyAfterCreateTexture() {
             if (AfterCreateTexture != null)
                 AfterCreateTexture (this);
+        }
+        static int ParseAntiAliasing (int antiAliasing) {
+            return (antiAliasing > 0 ? antiAliasing : (QualitySettings.antiAliasing == 0 ? 1 : QualitySettings.antiAliasing));
         }
 
         #region IDisposable implementation
