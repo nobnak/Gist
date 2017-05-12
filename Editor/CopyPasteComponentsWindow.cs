1c61e66ce3a99edaee46221decfe5a7da373d185
Nobuyuki Nakata <nobu.nakata@gmail.com>
2017-05-12 20:33:52 +0900

    オブジェクト間のコンポーネントコピー

---
+++ Editor/CopyPasteComponentsWindow.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Gist.Editor {
+
+    public class CopyPasteComponentsWindow : EditorWindow {
+        GameObject objectFrom;
+        GameObject objectTo;
+
+        [MenuItem("Custom/Window/CopyPastComponents")]
+        static void OpenWindow() {
+            EditorWindow.GetWindow (typeof(CopyPasteComponentsWindow));
+        }
+
+        void OnGUI() {
+            objectFrom = ObjectField("From", objectFrom, true);
+            objectTo = ObjectField ("To", objectTo, true);
+
+            GUI.enabled = (objectFrom != null && objectTo != null);
+            if (GUILayout.Button ("Copy"))
+                Copy (objectFrom, objectTo);
+            GUI.enabled = true;
+        }
+
+        static void Copy(GameObject objectFrom, GameObject objectTo) {
+            foreach (var comp in objectFrom.GetComponents<Component>()) {
+                UnityEditorInternal.ComponentUtility.CopyComponent (comp);
+                UnityEditorInternal.ComponentUtility.PasteComponentAsNew (objectTo);
+            }
+        }
+        static T ObjectField<T>(string label, T obj, bool allowSceneObject) where T:Object {
+            return (T)EditorGUILayout.ObjectField (label, obj, typeof(T), allowSceneObject);
+        }
+    }
+
+}
