d07bc188e83b0b431f6976639062089e9336f3b8
Nobuyuki Nakata <nobu.nakata@gmail.com>
2017-05-19 15:58:18 +0900

    UnityEvent 追加

--- UnityEvents.cs
+++ UnityEvents.cs
@@ -6,5 +6,6 @@ namespace Gist {
 
     [System.Serializable]
     public class TextureEvent : UnityEngine.Events.UnityEvent<Texture> {}
-
+    [System.Serializable]
+    public class Vector3Event : UnityEngine.Events.UnityEvent<Vector3> {}
 }
