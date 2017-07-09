05b668cb1df2f72454e3132eae892b306f3bd067
Naka Nobuyuki <nobu.nakata@gmail.com>
2017-07-09 15:49:21 +0900

    一通りのプロパティーについて、メソッドを用意

--- Events/ListenerMaterial.cs
+++ Events/ListenerMaterial.cs
@@ -6,31 +6,58 @@ namespace Gist.Events {
         
     public class ListenerMaterial : MonoBehaviour {
         [SerializeField]
+        protected string defaultColorName = "_Color";
+        [SerializeField]
+        protected string defaultFloatName = "_Float";
+        [SerializeField]
+        protected string defaultMatrixName = "_Matrix";
+        [SerializeField]
         protected string defaultTextureName = "_MainTex";
+        [SerializeField]
+        protected string defaultVectorName = "_Vector";
 
         protected MaterialPropertyBlockChanied block;
 
-        #region Unity
-        protected virtual void OnEnable() {
-            Renderer rend;
-            if ((rend = GetComponent<Renderer> ()) != null)
-                block = new MaterialPropertyBlockChanied (rend);
-        }
-        #endregion
-
-        public virtual void SetTexture(Texture tex) {
-            SetTexture(defaultTextureName, tex);
-        }
-        public virtual void SetTexture(string textureName, Texture tex) {
-            if (block != null)
-                block.SetTexture (textureName, tex).Apply ();
+        public virtual MaterialPropertyBlockChanied Block {
+            get { 
+                if (block == null)
+                    block = new MaterialPropertyBlockChanied (GetComponent<Renderer> ());
+                return block;
+            }
         }
 
-        public virtual Texture GetTexture(string textureName) {
-            return (block != null ? block.GetTexture (textureName) : null);
+        public virtual void Set(string colorName, Color value) {
+            Block.SetColor (colorName, value).Apply ();
         }
-        public virtual Texture GetTexture() {
-            return GetTexture (defaultTextureName);
+        public virtual void Set(string textureName, float value) {
+            Block.SetFloat (textureName, value).Apply ();
         }
+        public virtual void Set(string textureName, Matrix4x4 value) {
+            Block.SetMatrix (textureName, value).Apply ();
+        }
+        public virtual void Set(string textureName, Texture value) {
+            Block.SetTexture (textureName, value).Apply ();
+        }
+        public virtual void Set(string textureName, Vector4 value) {
+            Block.SetVector (textureName, value).Apply ();
+        }
+
+        public virtual void Set(Color value) { Set(defaultColorName, value); }
+        public virtual void Set(float value) { Set(defaultFloatName, value); }
+        public virtual void Set(Matrix4x4 value) { Set(defaultMatrixName, value); }
+        public virtual void Set(Texture value) { Set(defaultTextureName, value); }
+        public virtual void Set(Vector4 value) { Set(defaultVectorName, value); }
+
+        public virtual Color GetColor(string name) { return Block.GetColor (name); }
+        public virtual float GetFloat(string name) { return Block.GetFloat (name); }
+        public virtual Matrix4x4 GetMatrix(string name) { return Block.GetMatrix (name); }
+        public virtual Texture GetTexture(string name) { return Block.GetTexture (name); }
+        public virtual Vector4 GetVector(string name) { return Block.GetVector (name); }
+
+        public virtual Color GetColor() { return Block.GetColor (defaultColorName); }
+        public virtual float GetFloat() { return Block.GetFloat (defaultFloatName); }
+        public virtual Matrix4x4 GetMatrix() { return Block.GetMatrix (defaultMatrixName); }
+        public virtual Texture GetTexture() { return Block.GetTexture (defaultTextureName); }
+        public virtual Vector4 GetVector() { return Block.GetVector (defaultVectorName); }
     }
 }
