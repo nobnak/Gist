a0cd1fdd6ad1b6f0b02c9cb76ee49f1e0732ba49
Nobuyuki Nakata <nobu.nakata@gmail.com>
2017-08-30 17:40:04 +0900

    Float型イベントを追加

--- Events/UnityEvents.cs
+++ Events/UnityEvents.cs
@@ -7,6 +7,8 @@ namespace Gist.Events {
     [System.Serializable]
     public class TextureEvent : UnityEngine.Events.UnityEvent<Texture> {}
     [System.Serializable]
+    public class FloatEvent : UnityEngine.Events.UnityEvent<float> {}
+    [System.Serializable]
     public class Vector2Event : UnityEngine.Events.UnityEvent<Vector2> {}
     [System.Serializable]
     public class Vector3Event : UnityEngine.Events.UnityEvent<Vector3> {}
