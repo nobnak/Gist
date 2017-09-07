6ad5f67d2bd456d42c39b441d9939c97cc856f88
Nobuyuki Nakata <nobu.nakata@gmail.com>
2017-09-07 17:50:37 +0900

    計算の高速化

--- Generative/Util/TextureFilter.cs
+++ Generative/Util/TextureFilter.cs
@@ -10,10 +10,17 @@ namespace Gist {
             var lwidth = width - 1;
             var x = uv.x * lwidth;
             var y = uv.y * lwidth;
-            var ix = Mathf.Clamp ((int)x, 0, lwidth);
-            var iy = Mathf.Clamp ((int)y, 0, lwidth);
-            var jx = Mathf.Clamp (ix + 1, 0, lwidth);
-            var jy = Mathf.Clamp (iy + 1, 0, lwidth);
+
+            var ix = (int)x;
+            var iy = (int)y;
+            ix = (ix < 0 ? 0 : (ix <= lwidth ? ix : lwidth));
+            iy = (iy < 0 ? 0 : (iy <= lwidth ? iy : lwidth));
+
+            var jx = ix + 1;
+            var jy = iy + 1;
+            jx = (jx <= lwidth ? jx : lwidth);
+            jy = (jy <= lwidth ? jy : lwidth);
+
             var dx = x - ix;
             var dy = y - iy;
 
@@ -24,15 +31,31 @@ namespace Gist {
             var lwidth = width - 1;
             var x = uv.x * lwidth;
             var y = uv.y * lwidth;
-            var ix = Mathf.Clamp ((int)x, 0, lwidth);
-            var iy = Mathf.Clamp ((int)y, 0, lwidth);
-            var jx = Mathf.Clamp (ix + 1, 0, lwidth);
-            var jy = Mathf.Clamp (iy + 1, 0, lwidth);
-            var dx = x - ix;
-            var dy = y - iy;
 
-            return (1f - dx) * ((1f - dy) * Value (ix, iy) + dy * Value (ix, jy))
-                + dx * ((1f - dy) * Value (jx, iy) + dy * Value (jx, jy));
+            var ix = (int)x;
+            var iy = (int)y;
+            ix = (ix < 0 ? 0 : (ix <= lwidth ? ix : lwidth));
+            iy = (iy < 0 ? 0 : (iy <= lwidth ? iy : lwidth));
+
+            var jx = ix + 1;
+            var jy = iy + 1;
+            jx = (jx <= lwidth ? jx : lwidth);
+            jy = (jy <= lwidth ? jy : lwidth);
+
+            var dx1 = x - ix;
+            var dy1 = y - iy;
+            var dx0 = 1f - dx1;
+            var dy0 = 1f - dy1;
+
+            var vii = Value (ix, iy);
+            var vij = Value (ix, jy);
+            var vji = Value (jx, iy);
+            var vjj = Value (jx, jy);
+
+            return  new Vector3 (
+                dx0 * (dy0 * vii.x + dy1 * vij.x) + dx1 * (dy0 * vji.x + dy1 * vjj.x),
+                dx0 * (dy0 * vii.y + dy1 * vij.y) + dx1 * (dy0 * vji.y + dy1 * vjj.y),
+                dx0 * (dy0 * vii.z + dy1 * vij.z) + dx1 * (dy0 * vji.z + dy1 * vjj.z));
         }
     }
 }
