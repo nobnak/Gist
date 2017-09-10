0c740e90b7289ec3ea2dd02b2744d29f96b6dac2
Naka Nobuyuki <nobu.nakata@gmail.com>
2017-09-10 16:54:41 +0900

    リソース破棄の共通関数

---
+++ ObjectDestructor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gist {
+        
+    public static class ObjectDestructor {
+        public static void Destroy(Object obj) {
+            if (Application.isPlaying)
+                Object.Destroy (obj);
+            else
+                Object.DestroyImmediate (obj);
+        }
+    }
+}
