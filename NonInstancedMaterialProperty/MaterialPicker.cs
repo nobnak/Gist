29e0cdd195c9c9dae58f7c48e3794bb082df210a
Nobuyuki Nakata <nobu.nakata@gmail.com>
2017-09-27 21:48:40 +0900

    GPU Instancing 用の Material キャッシュの仕組みを作りたい

---
+++ NonInstancedMaterialProperty/MaterialPicker.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NonInstancedMaterialProperty {
+
+    public class MaterialPicker : MonoBehaviour {
+
+        public Property[] properties;
+
+        #region Unity
+        #endregion
+
+    }
+}
---
+++ NonInstancedMaterialProperty/Property.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NonInstancedMaterialProperty {
+
+    [System.Serializable]
+    public class Property {
+        public enum ValueTypeEnum { Texture = 0 }
+
+        public string name;
+        public ValueTypeEnum valueType;
+
+        public Texture[] textureValues;
+
+        public void Set(Material m, int valueIndex) {
+            m.SetTexture(name, textureValues[valueIndex]);
+        }
+        public int CountValues {
+            get {
+                return textureValues.Length;
+            }
+        }
+    }
+}
