bb499f61eed835f74d527f808c484bbdafdbb0ad
Nobuyuki Nakata <nobu.nakata@gmail.com>
2017-10-11 19:10:38 +0900

    Scoped オブジェクト追加

---
+++ Scoped/ScopedActivator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gist.Scoped {
+
+    public class ScopedRenderTextureActivator : ScopedActivator {
+        protected RenderTexture prev;
+
+        public ScopedRenderTextureActivator(RenderTexture next) {
+            prev = RenderTexture.active;
+            RenderTexture.active = next;
+        }
+
+        #region IDisposable implementation
+        public override void Dispose () {
+            RenderTexture.active = prev;
+        }
+        #endregion
+    }
+
+    public abstract class ScopedActivator : System.IDisposable {
+        #region IDisposable implementation
+        public abstract void Dispose ();
+        #endregion
+        
+    }
+}
---
+++ Scoped/ScopedObject.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gist.Scoped {
+
+    public class ScopedObject<T> : ScopedObject where T : Object {
+        public T Data { get; protected set; }
+
+        public ScopedObject(T data) {
+            this.Data = data;
+            this.Disposed = false;
+        }
+
+        public override void Dispose() {
+            if (!Disposed) {
+                Disposed = true;
+                Release(Data);
+            }
+        }
+
+        public static implicit operator ScopedObject<T>(T data) {
+            return new ScopedObject<T>(data);
+        }
+        public static implicit operator T(ScopedObject<T> scoped) {
+            return scoped.Data;
+        }
+    }
+
+    public abstract class ScopedObject : System.IDisposable {
+        public bool Disposed { get; protected set; }
+
+        public static void Release(Object obj) {
+            if (Application.isPlaying)
+                Object.Destroy(obj);
+            else
+                Object.DestroyImmediate(obj);
+        }
+
+        public abstract void Dispose();
+    }
+}
