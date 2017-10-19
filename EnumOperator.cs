b5cfcd4787c67b075392b69fede5963b42bd5e8f
Nobuyuki Nakata <nobu.nakata@gmail.com>
2017-10-19 20:13:41 +0900

    Enum の数値オペレーター作る

---
+++ EnumOperator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Gist {
+
+    public static class EnumOperator<T> {
+        public static readonly int MIN = System.Enum.GetValues(typeof(T)).Cast<int>().Min();
+        public static readonly int MAX = System.Enum.GetValues(typeof(T)).Cast<int>().Max();
+
+        public static T Repeat(int i) {
+            i = (i < MIN ? MAX : (i <= MAX ? i : MIN));
+            return (T)(object)i;
+        }
+    }
+}
