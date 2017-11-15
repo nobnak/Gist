97bf3fe44a482caac4a39a28b0124e043d954730
Nobuyuki Nakata <nobu.nakata@gmail.com>
2017-11-15 21:49:51 +0900

    Add extension for behaviour

---
+++ Extensions/BehaviourExtension.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gist.Extensions.Behaviour {
+
+    public static class BehaviourExtension {
+
+        public static bool IsActiveAndEnabledAlsoInEditMode(this MonoBehaviour b) {
+            return (Application.isPlaying ? b.isActiveAndEnabled : b.runInEditMode);
+        }
+    }
+}
