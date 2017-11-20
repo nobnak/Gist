7bb072172911dc055d0f66ebe0db7425bceefaea
Nobuyuki Nakata <nobu.nakata@gmail.com>
2017-11-20 21:31:34 +0900

    Validator にリセット機能

--- Validator.cs
+++ Validator.cs
@@ -8,18 +8,24 @@ namespace Gist {
 
         public event System.Action Validation;
 
+        protected bool initialValidity;
         protected bool internalValidity;
         protected System.Func<bool> extraValidity;
 
         public Validator(bool initialValidity) {
+            this.initialValidity = initialValidity;
             this.internalValidity = initialValidity;
         }
         public Validator() : this(false) { }
 
+        public void Reset() {
+            internalValidity = initialValidity;
+            Validation = null;
+            extraValidity = null;
+        }
         public void SetExtraValidityChecker(System.Func<bool> extraValidity) {
             this.extraValidity = extraValidity;
         }
-
         public bool Valid {
             get { return internalValidity && (extraValidity == null || extraValidity()); }
         }
