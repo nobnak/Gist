a8857a0d0bea8df963c8e47f9b546c82c4ea9fac
Nobuyuki Nakata <nobu.nakata@gmail.com>
2017-11-27 21:20:32 +0900

    誤って削除した SpecialFolder 拡張クラスを再追加

---
+++ Extensions/SpecialFolderExtension.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+namespace Gist.Extensions.SpecialFolder {
+
+    public static class SpecialFolderExtension {
+        
+        public static string DataPath(this System.Environment.SpecialFolder folder, string filename) {
+            var dir = System.Environment.GetFolderPath(folder);
+            return Path.Combine(dir, filename);
+        }
+        public static bool Load<S>(this System.Environment.SpecialFolder folder, string filename, ref S data) {
+            try {
+                var path = folder.DataPath(filename);
+                JsonUtility.FromJsonOverwrite(File.ReadAllText(path), data);
+                return true;
+            } catch (System.Exception e) {
+                Debug.Log(e);
+            }
+            return false;
+        }
+        public static bool Save<S>(this System.Environment.SpecialFolder folder, string filename, ref S data) {
+            try {
+                var path = folder.DataPath(filename);
+                File.WriteAllText(path, JsonUtility.ToJson(data, true));
+                return true;
+            } catch (System.Exception e) {
+                Debug.Log(e);
+            }
+            return false;
+        }
+    }
+}
