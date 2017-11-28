b339259178ae767c81018891d275709ce58496d0
Nobuyuki Nakata <nobu.nakata@gmail.com>
2017-11-28 16:43:57 +0900

    ビルドエラー修正

--- Extensions/BehaviourExtension.cs
+++ Extensions/BehaviourExtension.cs
@@ -7,7 +7,11 @@ namespace Gist.Extensions.Behaviour {
     public static class BehaviourExtension {
 
         public static bool IsActiveAndEnabledAlsoInEditMode(this MonoBehaviour b) {
+            #if UNITY_EDITOR
             return (Application.isPlaying ? b.isActiveAndEnabled : b.runInEditMode);
+            #else
+            return b.isActiveAndEnabled;
+            #endif
         }
     }
 }
