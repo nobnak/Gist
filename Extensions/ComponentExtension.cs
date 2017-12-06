268a18cad72f353aa697e3bbe7ce9697a8e4a866
Nobuyuki Nakata <nobu.nakata@gmail.com>
2017-12-06 21:28:57 +0900

    Component Extension 追加

---
+++ Extensions/ComponentExtension.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gist.Extensions.ComponentExt {
+
+    public static class ComponentExtension {
+        public static IEnumerable<T> AggregateComponentsInChildren<T>(this Transform parent)
+            where T : Component {
+
+            if (parent == null)
+                yield break;
+
+            for (var i = 0; i < parent.childCount; i++) {
+                var child = parent.GetChild(i);
+                var comp = child.GetComponent<T>();
+                if (comp != null)
+                    yield return comp;
+                else
+                    foreach (var c in child.AggregateComponentsInChildren<T>())
+                        yield return c;
+            }
+        }
+    }
+
+}
