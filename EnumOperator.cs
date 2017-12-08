41ff944246f2bb5c2ea5ac264109f795bb34e00c
Nobuyuki Nakata <nobu.nakata@gmail.com>
2017-12-08 18:24:44 +0900

    機能追加

--- EnumOperator.cs
+++ EnumOperator.cs
@@ -1,17 +1,35 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Blending.Samples;
 using UnityEngine;
 
 namespace Gist {
 
-    public static class EnumOperator<T> {
-        public static readonly int MIN = System.Enum.GetValues(typeof(T)).Cast<int>().Min();
-        public static readonly int MAX = System.Enum.GetValues(typeof(T)).Cast<int>().Max();
+    public static class EnumOperator<T> where T : IComparable {
+
+        public static readonly string[] NAMES = System.Enum.GetNames(typeof(T));
+        public static readonly T[] VALUES = System.Enum.GetValues(typeof(T)).Cast<T>().ToArray();
+        public static readonly int[] INDICES = VALUES.Cast<int>().ToArray();
+
+        public static readonly int MIN = INDICES.Min();
+        public static readonly int MAX = INDICES.Max();
 
         public static T Repeat(int i) {
             i = (i < MIN ? MAX : (i <= MAX ? i : MIN));
             return (T)(object)i;
+        }
+
+        public static T ValueAt(int index) {
+            return VALUES[index];
+        }
+        public static string NameAt(int index) {
+            return NAMES[index];
+        }
+
+        internal static int FindIndex(T value) {
+            return VALUES.Select((v,i)=>i).Where(i => VALUES[i].CompareTo(value) == 0).FirstOrDefault();
         }
     }
 }
