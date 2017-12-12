53f49ab716e67e1a46e8f98648e8f1b3982bb47d
Naka Nobuyuki <nobu.nakata@gmail.com>
2017-12-13 03:03:56 +0900

    Revert "Remove editor scripts"
    This reverts commit 008d74b0df72f6b78ffeb86338b686b14129a969.

---
+++ Extensions/Editor/AABBExtensionTest.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+namespace nobnak.Gist.Extensions.AABB.Test {
+
+    public class AABBExtensionTest {
+        public const float EPSILON = 1e-3f;
+
+        [Test]
+        public void AABBExtensionTestSimplePasses() {
+            var rect = new Rect(0f, 0f, 1f, 1f);
+
+            var mat2 = Matrix4x4.identity;
+            var rect2 = rect.EncapsulateInTargetSpace(mat2);
+            Debug.LogFormat("Rect in={0} out={1}", rect, rect2);
+            Assert.AreEqual(rect.center.x, rect2.center.x, EPSILON);
+            Assert.AreEqual(rect.center.y, rect2.center.y, EPSILON);
+        }
+    }
+}
---
+++ GPUBuffer/Editor/TestGPUList.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace nobnak.Gist.GPUBuffer {
+
+    public class TestGPUList {
+
+        [Test]
+        public void TestGPUListSimplePasses() {
+            var length = 16;
+            var listA = new GPUList<int>();
+
+            var data = new List<int>();
+            for (var i = 0; i < length; i++) {
+                var v = Random.Range(0, 9);
+                data.Add(v);
+                listA.Add(v);
+                Assert.AreEqual(v, listA[i]);
+                Assert.AreEqual(listA.Count, i + 1);
+            }
+
+            Assert.AreEqual(data.Count, listA.Count);
+            Assert.True(listA.Upload());
+            listA.Download();
+
+            for (var i = 0; i < data.Count; i++) {
+                Assert.AreEqual(data[i], listA[i]);
+            }
+
+            for (var i = 0; i < length; i++) {
+                var j = Random.Range(0, data.Count - 1);
+                data.RemoveAt(j);
+                listA.RemoveAt(j);
+                AreEquals(data, listA);
+            }
+
+            listA.Dispose();
+        }
+
+        private static void AreEquals<T>(IList<T> a, IList<T> b) {
+            Assert.AreEqual(a.Count, b.Count);
+            for (var i = 0; i < a.Count; i++)
+                Assert.AreEqual(a[i], b[i]);
+        }
+    }
+}
