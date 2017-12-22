061eccb980aaf8c4a2b07c70753dff2bca07117f
Nobuyuki Nakata <nobu.nakata@gmail.com>
2017-12-22 21:25:37 +0900

    データを一定時間保持するクラス

---
+++ TemporalCache.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gist {
+
+    public class TemporalCache<V> {
+
+        protected float cacheDuration;
+        protected float valueGeneratedTime;
+        protected V value;
+
+        public TemporalCache(float cacheDuration) {
+            this.valueGeneratedTime = float.MinValue;
+            this.cacheDuration = cacheDuration;
+        }
+
+        public V Wrap(V nextValue) {
+            if (!ValueIsEffective)
+                SetValue(nextValue);
+            return value;
+        }
+
+        protected bool ValueIsEffective {
+            get { return (valueGeneratedTime + cacheDuration) >= Time.time; }
+        }
+        protected void SetValue(V value) {
+            this.value = value;
+            this.valueGeneratedTime = Time.time;
+        }
+    }
+}
