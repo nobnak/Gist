0e0ee1e64c111b20658a8adf3136ca14e202efdc
Nakata Nobuyuki <nobu.nakata@gmail.com>
2018-01-25 19:07:50 +0900

    OBB2 に Rect 系のメソッドを追加

---
+++ Intersection/2D/IConvex2Distance.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace nobnak.Gist.Intersection {
+
+    public interface IConvex2Distance {
+
+        Vector2 ClosestPoint(Vector2 point);
+        bool Contains(Vector2 point);
+    }
+}
--- Intersection/2D/OBB2.cs
+++ Intersection/2D/OBB2.cs
@@ -1,24 +1,33 @@
-﻿using nobnak.Gist.Extensions.AABB;
+﻿using Gist.Extensions.RectExt;
+using nobnak.Gist.Extensions.AABB;
+using nobnak.Gist.Primitive;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace nobnak.Gist.Intersection {
 
-    public class OBB2 : IConvex2Polytope {
+    public class OBB2 : IConvex2Polytope, IConvex2Distance {
         public static readonly Vector2[] VERTICES = new Vector2[] {
             0.5f * new Vector2(-1,-1), 0.5f * new Vector2(-1,1),
             0.5f * Vector2.one, 0.5f * new Vector2(1,-1)
         };
-        public static readonly Rect QUAD = new Rect(-0.5f, -0.5f, 1f, 1f);
+        public static readonly Rect LOCAL_BOUNDS = new Rect(-0.5f, -0.5f, 1f, 1f);
+        public static readonly Vector2 LOCAL_MIN = LOCAL_BOUNDS.min;
+        public static readonly Vector2 LOCAL_MAX = LOCAL_BOUNDS.max;
 
-        protected Matrix4x4 model;
-        protected Rect bounds;
+        protected DefferedMatrix model = new DefferedMatrix();
+        protected Rect worldBounds;
 
         protected Vector2 xaxis;
         protected Vector2 yaxis;
 
+        public OBB2() { }
         public OBB2(Vector2 center, Vector2 size, Vector2 xaxis) {
+            Reset(center, size, xaxis);
+        }
+
+        public virtual void Reset(Vector2 center, Vector2 size, Vector2 xaxis) {
             var yaxis = new Vector2(-xaxis.y, xaxis.x);
             this.xaxis = xaxis;
             this.yaxis = yaxis;
@@ -26,14 +35,18 @@ namespace nobnak.Gist.Intersection {
             xaxis *= size.x;
             yaxis *= size.y;
 
-            model = Matrix4x4.zero;
-            model[0] = xaxis.x; model[4] = yaxis.x; model[12] = center.x;
-            model[1] = xaxis.y; model[5] = yaxis.y; model[13] = center.y;
-            model[15] = 1f;
-
-            bounds = QUAD.EncapsulateInTargetSpace(model);
+            var localToWorld = Matrix4x4.zero;
+            localToWorld[0] = xaxis.x; localToWorld[4] = yaxis.x; localToWorld[12] = center.x;
+            localToWorld[1] = xaxis.y; localToWorld[5] = yaxis.y; localToWorld[13] = center.y;
+            localToWorld[15] = 1f;
+            Reset(localToWorld);
+        }
+        public void Reset(Matrix4x4 localToWorld) {
+            model.Reset(localToWorld);
+            worldBounds = LOCAL_BOUNDS.EncapsulateInTargetSpace(localToWorld);
         }
 
+        #region IConvex2Polytope
         public IEnumerable<Vector2> Normals() {
             yield return xaxis;
             yield return yaxis;
@@ -41,13 +54,37 @@ namespace nobnak.Gist.Intersection {
 
         public IEnumerable<Vector2> Vertices() {
             foreach (var v in VERTICES)
-                yield return model.MultiplyPoint3x4(v);
+                yield return model.Matrix.MultiplyPoint3x4(v);
         }
 
         public Rect WorldBounds {
-            get { return bounds; }
+            get { return worldBounds; }
         }
 
-        public Matrix4x4 Model { get { return model; } }
+        public Matrix4x4 Model { get { return model.Matrix; } }
+        #endregion
+
+        #region IConvex2Distance
+        public virtual Vector2 ClosestPoint(Vector2 worldPoint) {
+            var localPoint = (Vector2)model.InverseTransformPoint(worldPoint);
+            var localClosestOne = LOCAL_BOUNDS.ClosestPoint(localPoint);
+            var worldClosestOne = model.TransformPoint(localClosestOne);
+            return worldClosestOne;
+        }
+        public virtual bool Contains(Vector2 worldPoint) {
+            var localPoint = (Vector2)model.InverseTransformPoint(worldPoint);
+            return Contains(LOCAL_MIN, LOCAL_MAX, localPoint);
+        }
+        public static bool Contains(Vector2 min, Vector2 max, Vector2 point) {
+            var minx = min.x;
+            var miny = min.y;
+            var maxx = max.x;
+            var maxy = max.y;
+            var px = point.x;
+            var py = point.y;
+
+            return minx <= px && px < maxx && miny <= py && py < maxy;
+        }
+        #endregion
     }
 }
