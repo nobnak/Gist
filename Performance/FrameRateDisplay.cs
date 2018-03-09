25daeef540eeb9eda464a2072c431b68c6190411
Nakata Nobuyuki <nobu.nakata@gmail.com>
2018-03-09 18:41:34 +0900

    PFS 表示機能

---
+++ Performance/FrameRateDisplay.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gist.Performance {
+
+    public class FrameRateDisplay : MonoBehaviour {
+        public KeyCode guiDisplayKey = KeyCode.F;
+        public float updateFreq = 0.5f;
+
+        protected float cachedFrameRate;
+
+        protected bool visible;
+        protected GUIStyle style;
+        protected Rect windowRect;
+
+        #region Unity
+        private void OnEnable() {
+            windowRect = new Rect(10f, 10f, 10f, 10f);
+            StartCoroutine(Job());
+        }
+        private void Update() {
+            if (Input.GetKeyDown(guiDisplayKey)) {
+                visible = !visible;
+            }
+        }
+        private void OnGUI() {
+            if (visible)
+                windowRect = GUILayout.Window(GetInstanceID(), windowRect, Window, GetType().Name);
+        }
+        #endregion
+
+        protected virtual IEnumerator Job() {
+            while (true) {
+                cachedFrameRate = 1.0f / Time.smoothDeltaTime;
+                yield return new WaitForSeconds(updateFreq);
+            }
+        }
+        protected virtual void Window(int id) {
+            if (style == null) {
+                style = new GUIStyle(GUI.skin.label);
+                style.stretchWidth = true;
+                style.wordWrap = false;
+            }
+
+            using (new GUILayout.VerticalScope()) {
+                using (new GUILayout.HorizontalScope()) {
+                    GUILayout.Label(string.Format("Frame-rate : {0:f1} (fps)", cachedFrameRate), style);
+                }
+            }
+
+            GUI.DragWindow();
+        }
+    }
+}
