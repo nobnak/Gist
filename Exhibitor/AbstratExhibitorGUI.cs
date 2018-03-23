299baf00e57ee297aa052af3d410d7a55bb0eef0
Nakata Nobuyuki <nobu.nakata@gmail.com>
2018-03-23 20:27:00 +0900

    IExhibitor の GUI 用ベースクラスを定義

---
+++ Exhibitor/AbstratExhibitorGUI.cs
@@ -0,0 +1,22 @@
+using nobnak.Gist.Extensions.ComponentExt;
+using nobnak.Gist.Layer2;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using nobnak.Gist.ObjectExt;
+
+namespace nobnak.Gist.Exhibitor {
+
+	public abstract class AbstratExhibitorGUI : MonoBehaviour, IExhibitor {
+
+		#region IExhibitor
+		public abstract void Invalidate();
+		public abstract string SerializeToJson();
+		public abstract void DeserializeFromJson(string json);
+		#endregion
+
+		#region GUI
+		public abstract void Draw();
+		#endregion
+	}
+}
