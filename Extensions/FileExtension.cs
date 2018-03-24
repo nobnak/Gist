eb586797010371838a6b6392a5e30609f302cc47
Nakata Nobuyuki <nobu.nakata@gmail.com>
2018-03-24 22:56:27 +0900

    ファイルアクセス拡張メソッド

---
+++ Extensions/FileExtension.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace nobnak.Gist.Extensions.FileExt {
+
+	public static class FileExtension {
+
+		public static bool TrySave(this string path, string text) {
+			try {
+				File.WriteAllText(path, text);
+				return true;
+			} catch {
+				return false;
+			}
+		}
+		public static bool TryLoad(this string path, out string text) {
+			try {
+				text = File.ReadAllText(path);
+				return true;
+			} catch {
+				text = default(string);
+				return false;
+			}
+		}
+
+		public static void Save(this string path, string text) {
+			path.TrySave(text);
+		}
+		public static string Load(this string path) {
+			string text;
+			path.TryLoad(out text);
+			return text;
+		}
+	}
+}
