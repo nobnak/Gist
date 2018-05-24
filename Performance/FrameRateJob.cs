744695af539881f9f5ab203b17b05e66af605589
Nakata Nobuyuki <nobu.nakata@gmail.com>
2018-05-24 19:00:58 +0900

    ファイルの整理

---
+++ Performance/FrameRateJob.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gist.Performance {
+
+	[System.Serializable]
+    public class FrameRateJob : IEnumerable {
+		[SerializeField]
+        protected float updateFreq = 0.5f;
+
+        protected float currFramerate;
+		protected int currRefreshrate;
+
+		#region public
+		public float UpdateFreq {
+			get { return updateFreq; }
+			set {
+				updateFreq = Mathf.Max(0f, value);
+			}
+		}
+		public float CurrentFrameRate {
+			get { return currFramerate; }
+		}
+		public int CurrentRefreshRate {
+			get { return currRefreshrate; }
+		}
+
+		public override string ToString() {
+			return string.Format(
+				"Frame-rate : {0:f1} (fps) / {1}",
+				currFramerate, currRefreshrate);
+		}
+		#endregion
+
+		#region IEnumerable
+		public IEnumerator GetEnumerator() {
+			while (true) {
+				try {
+					currFramerate = 1.0f / Time.smoothDeltaTime;
+					var currResolution = Screen.currentResolution;
+					currRefreshrate = currResolution.refreshRate;
+				} catch { }
+				yield return new WaitForSeconds(updateFreq);
+			}
+		}
+		#endregion
+	}
+}
