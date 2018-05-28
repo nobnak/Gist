a661b9d4a9e88a6ef43b0d8b1c7db3024d210dd5
Nakata Nobuyuki <nobu.nakata@gmail.com>
2018-05-28 16:24:25 +0900

    long 系に対応

--- Extensions/IntExtension.cs
+++ Extensions/IntExtension.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,14 +6,56 @@ namespace nobnak.Gist.Extensions.Int {
     
     public static class IntExtension {
 
-        public static int SmallestPowerOfTwoGreaterThan(this int n) {
-            --n;
-            n |= n >> 1;
-            n |= n >> 2;
-            n |= n >> 4;
-            n |= n >> 8;
-            n |= n >> 16;
-            return ++n;
-        }
-    }
+		public static int SmallestPowerOfTwoGreaterThan(this int n) {
+			if (n <= 0)
+				return 0;
+
+			--n;
+			n |= n >> 1;
+			n |= n >> 2;
+			n |= n >> 4;
+			n |= n >> 8;
+			n |= n >> 16;
+			return ++n;
+		}
+		public static uint SmallestPowerOfTwoGreaterThan(this uint n) {
+			if (n <= 0)
+				return 0;
+
+			--n;
+			n |= n >> 1;
+			n |= n >> 2;
+			n |= n >> 4;
+			n |= n >> 8;
+			n |= n >> 16;
+			return ++n;
+		}
+
+		public static long SmallestPowerOfTwoGreaterThan(this long n) {
+			if (n <= 0)
+				return 0;
+
+			--n;
+			n |= n >> 1;
+			n |= n >> 2;
+			n |= n >> 4;
+			n |= n >> 8;
+			n |= n >> 16;
+			n |= n >> 32;
+			return ++n;
+		}
+		public static ulong SmallestPowerOfTwoGreaterThan(this ulong n) {
+			if (n <= 0)
+				return 0;
+
+			--n;
+			n |= n >> 1;
+			n |= n >> 2;
+			n |= n >> 4;
+			n |= n >> 8;
+			n |= n >> 16;
+			n |= n >> 32;
+			return ++n;
+		}
+	}
 }
