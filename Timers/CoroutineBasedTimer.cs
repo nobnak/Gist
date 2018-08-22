1e9649bdf1bc02dd7085d323bece0aa8116143ad
Nakata Nobuyuki <nobu.nakata@gmail.com>
2018-08-22 10:15:57 +0300

    コルーチンタイマーの追加

---
+++ Timers/CoroutineBasedTimer.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace nobnak.Gist.Timers {
+
+	public class CoroutineBasedTimer : System.IDisposable {
+
+		public event System.Action Started;
+		public event System.Action Ended;
+
+		public event System.Action Completed;
+		public event System.Action Aborted;
+
+		protected MonoBehaviour target;
+		protected Coroutine current;
+
+		public CoroutineBasedTimer(MonoBehaviour target) {
+			this.target = target;
+		}
+
+		#region public
+		public CoroutineBasedTimer Start(float duration) {
+			Abort();
+			current = target.StartCoroutine(GetTimer(duration));
+			Started?.Invoke();
+			return this;
+		}
+		public CoroutineBasedTimer Abort() {
+			if (current != null) {
+				target.StopCoroutine(current);
+				current = null;
+				Aborted?.Invoke();
+				Ended?.Invoke();
+			}
+			return this;
+		}
+		#endregion
+
+		#region private
+		protected IEnumerator GetTimer(float duration) {
+			yield return new WaitForSeconds(duration);
+			Complete();
+		}
+		protected void Complete() {
+			current = null;
+			Completed?.Invoke();
+			Ended?.Invoke();
+		}
+		#endregion
+
+		#region IDisposable
+		public void Dispose() {
+			Abort();
+		}
+		#endregion
+	}
+}
