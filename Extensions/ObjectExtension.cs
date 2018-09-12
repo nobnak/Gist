cc8b1f08f11384df8efc2e6290cd6bf34aca0120
Nakata Nobuyuki <nobu.nakata@gmail.com>
2018-09-12 17:08:18 +0900

    オブジェクトのディープコピー拡張

--- Extensions/ObjectExtension.cs
+++ Extensions/ObjectExtension.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 
 namespace nobnak.Gist.ObjectExt {
-        
+
     public static class ObjectExtension {
         public static void Destroy(this Object obj, float t = 0f) {
 			if (obj != null) {
@@ -16,6 +18,10 @@ namespace nobnak.Gist.ObjectExt {
 		public static void Destroy(this Component comp, float t = 0f) {
 			if (comp != null)
 				comp.gameObject.Destroy(t);
+		}
+		public static T DeepCopy<T>(this T src) {
+			var json = JsonUtility.ToJson(src);
+			return JsonUtility.FromJson<T>(json);
 		}
     }
 }
