75e28c8cfd9927de7a9874e9d531523a9a2924d1
Nakata Nobuyuki <nobu.nakata@gmail.com>
2018-10-04 20:09:03 +0900

    オブジェクトの場所を検索

--- Extensions/ObjectExtension.cs
+++ Extensions/ObjectExtension.cs
@@ -4,6 +4,10 @@ using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 namespace nobnak.Gist.ObjectExt {
 
     public static class ObjectExtension {
@@ -23,5 +27,32 @@ namespace nobnak.Gist.ObjectExt {
 			var json = JsonUtility.ToJson(src);
 			return JsonUtility.FromJson<T>(json);
 		}
-    }
+
+#if UNITY_EDITOR
+		public static string AssetFolderName(this Object obj) {
+			string folder = null;
+			try {
+				folder = Path.GetDirectoryName(AssetDatabase.GetAssetPath(obj));
+				while (!Directory.Exists(folder)) {
+					if (string.IsNullOrEmpty(folder)) {
+						folder = "Assets";
+						break;
+					}
+					folder = Directory.GetParent(folder).FullName;
+				}
+			} catch {
+				folder = "Assets";
+			}
+
+			return folder;
+		}
+		public static IEnumerable<T> GetAssets<T>(this Object obj) where T : Object {
+			var path = AssetDatabase.GetAssetPath(obj);
+			foreach (var asset in AssetDatabase.LoadAllAssetsAtPath(path)) {
+				if (asset is T)
+					yield return (T)asset;
+			}
+		}
+#endif
+	}
 }
