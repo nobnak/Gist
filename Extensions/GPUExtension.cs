f97ac3bb058b625c9598da9d70616a0f7cbb15ed
Nakata Nobuyuki <nobu.nakata@gmail.com>
2019-05-22 16:12:46 +0900

    Compute Shader の Dispatch Size 計算

--- Extensions/GPUExtension.cs
+++ Extensions/GPUExtension.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace nobnak.Gist.Extensions.GPUExt {
@@ -8,6 +6,22 @@ namespace nobnak.Gist.Extensions.GPUExt {
 
 		public static int DispatchSize(this int threadCount, int groupSize) {
 			return (threadCount - 1) / groupSize + 1;
+		}
+		public static Vector3Int DispatchSize(this ComputeShader cs, int kernel, Vector3Int counts) {
+			uint x, y, z;
+			cs.GetKernelThreadGroupSizes(kernel, out x, out y, out z);
+			return new Vector3Int(
+				DispatchSize(counts.x, (int)x),
+				DispatchSize(counts.y, (int)y),
+				DispatchSize(counts.z, (int)z));
+		}
+		public static Vector3Int CeilSize(this ComputeShader cs, int kernel, Vector3Int counts) {
+			uint gsx, gsy, gsz;
+			cs.GetKernelThreadGroupSizes(kernel, out gsx, out gsy, out gsz);
+			return new Vector3Int(
+				(int)gsx * DispatchSize(counts.x, (int)gsx),
+				(int)gsy * DispatchSize(counts.y, (int)gsy),
+				(int)gsz * DispatchSize(counts.z, (int)gsz));
 		}
 	}
 }
