27365abfd4ca3f2d70f22abb8e6c3e07a323ede0
Nakata Nobuyuki <nobu.nakata@gmail.com>
2019-06-17 14:33:08 +0900

    DispatchSize 関数を追加

--- Extensions/GPUExtension.cs
+++ Extensions/GPUExtension.cs
@@ -15,13 +15,16 @@ namespace nobnak.Gist.Extensions.GPUExt {
 				DispatchSize(counts.y, (int)y),
 				DispatchSize(counts.z, (int)z));
 		}
-		public static Vector3Int CeilSize(this ComputeShader cs, int kernel, Vector3Int counts) {
+		public static Vector3Int DispatchSize(this ComputeShader cs, int kernel, int x, int y = 1, int z = 1) {
+			return cs.DispatchSize(kernel, new Vector3Int(x, y, z));
+		}
+		public static Vector3Int CeilSize(this ComputeShader cs, int kernel, int x, int y = 1, int z = 1) {
 			uint gsx, gsy, gsz;
 			cs.GetKernelThreadGroupSizes(kernel, out gsx, out gsy, out gsz);
 			return new Vector3Int(
-				(int)gsx * DispatchSize(counts.x, (int)gsx),
-				(int)gsy * DispatchSize(counts.y, (int)gsy),
-				(int)gsz * DispatchSize(counts.z, (int)gsz));
+				(int)gsx * DispatchSize(x, (int)gsx),
+				(int)gsy * DispatchSize(y, (int)gsy),
+				(int)gsz * DispatchSize(z, (int)gsz));
 		}
 	}
 }
