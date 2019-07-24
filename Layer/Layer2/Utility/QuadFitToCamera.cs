61402e81448ae59b4999f7c64d46d1b6f72d2941
Nakata Nobuyuki <nobu.nakata@gmail.com>
2019-07-24 19:36:06 +0900

    Quad をカメラサイズにするクラス

---
+++ Layer/Layer2/Utility/QuadFitToCamera.cs
@@ -0,0 +1,30 @@
+﻿using nobnak.Gist.Extensions.ComponentExt;
+using nobnak.Gist.Layer2;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace nobnak.Gist.Layer2 {
+
+    [ExecuteAlways]
+    public class QuadFitToCamera : MonoBehaviour {
+        [SerializeField]
+        protected Camera target;
+
+        #region interface
+        private void Update() {
+            if (target != null) {
+                var h = target.orthographicSize * 2f;
+                var w = target.aspect * h;
+
+                var s1 = new Vector3(w, h, 1f);
+                var s0 = transform.localScale;
+                if (s0 != s1) {
+                    transform.localScale = s1;
+                    this.CallbackSelf<IChangeListener<Transform>>(v => v.TargetOnChange(transform));
+                }
+            }
+        }
+        #endregion
+    }
+}
