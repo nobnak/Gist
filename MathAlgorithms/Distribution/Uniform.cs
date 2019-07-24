0eb4fc982879d1a9c687d38db3dd2d5caef09165
Nakata Nobuyuki <nobu.nakata@gmail.com>
2019-07-24 19:36:23 +0900

    Uniform サンプリングのエクステンション

---
+++ MathAlgorithms/Distribution/Uniform.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace nobnak.Gist.MathAlgorithms.Distribution.Uniform {
+
+    public static class UniformExtension {
+
+        public static float SUniform(this float d) {
+            return Random.Range(-d, d);
+        }
+
+        public static float UUniform(this float d) {
+            return Random.Range(0f, d);
+        }
+    }
+}
