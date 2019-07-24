433dd970e0efc35de6bbbcc5c83a3dcb7ae82cf3
Nakata Nobuyuki <nobu.nakata@gmail.com>
2019-07-24 19:35:36 +0900

    乱数の拡張メソッドを追加

---
+++ Extensions/RandomExtension.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace nobnak.Gist.Extensions.RandomExt {
+
+    public static class RandomFunction {
+
+        public static float RandomSNorm() {
+            return Random.Range(-1f, 1f);
+        }
+    }
+}
