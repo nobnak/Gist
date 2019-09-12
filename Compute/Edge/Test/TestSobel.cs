4dacf83e73812f9ace37384b0d0e48b58eb795cd
Nakata Nobuyuki <nobu.nakata@gmail.com>
2019-09-13 01:00:54 +0900

    ソーベルフィルタの実装

---
+++ Compute/Edge/Test/TestSobel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace nobnak.Gist.Compute.Edge {
+
+    [ExecuteAlways]
+    public class TestSobel : MonoBehaviour {
+
+        public enum OutputMode { ___ = 0, Monochrome, Overlay }
+
+        [SerializeField]
+        protected Material mat;
+        [SerializeField]
+        protected OutputMode outputMode;
+
+        void OnRenderImage(RenderTexture source, RenderTexture destination) {
+            mat.shaderKeywords = null;
+            if (outputMode != OutputMode.___)
+                mat.EnableKeyword(outputMode.ToString());
+
+            Graphics.Blit(source, destination, mat);
+        }
+    }
+}
