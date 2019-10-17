ed24839f2c4eba809b462baa6249a6f4d1aa0215
Nakata Nobuyuki <nobu.nakata@gmail.com>
2019-10-17 15:48:26 +0900

    Trigger のコールバックをイベント化

---
+++ Events/RigidbodyListener.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace nobnak.Gist.Events {
+
+    [ExecuteAlways]
+    [RequireComponent(typeof(Rigidbody))]
+    public class RigidbodyListener : MonoBehaviour {
+        [SerializeField]
+        protected ColliderEvent TriggerEnter = new ColliderEvent();
+        [SerializeField]
+        protected ColliderEvent TriggerStay = new ColliderEvent();
+        [SerializeField]
+        protected ColliderEvent TriggerExit = new ColliderEvent();
+
+        private void OnTriggerEnter(Collider other) {
+            TriggerEnter.Invoke(other);
+        }
+        private void OnTriggerStay(Collider other) {
+            TriggerStay.Invoke(other);
+        }
+        private void OnTriggerExit(Collider other) {
+            TriggerExit.Invoke(other);
+        }
+
+        #region classes
+        [System.Serializable]
+        public class ColliderEvent : UnityEngine.Events.UnityEvent<Collider> { }
+        #endregion
+    }
+}
