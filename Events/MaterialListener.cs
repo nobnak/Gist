b387bc3608b8ab05eadedbe7f7b15b77c9eee2f7
Nakata Nobuyuki <nobu.nakata@gmail.com>
2019-10-28 14:39:39 +0900

    Material が未設定の場合に、sharedmaterla から取得する

--- Events/MaterialListener.cs
+++ Events/MaterialListener.cs
@@ -8,7 +8,17 @@ namespace nobnak.Gist.Events {
 		[SerializeField]
 		protected Material targetMaterial;
 
-		public override void Set(string name, Color value) { targetMaterial.SetColor(name, value); }
+        #region unity
+        private void OnEnable() {
+            if (targetMaterial == null) {
+                var r = GetComponent<Renderer>();
+                if (r != null)
+                    targetMaterial = r.sharedMaterial;
+            }
+        }
+        #endregion
+
+        public override void Set(string name, Color value) { targetMaterial.SetColor(name, value); }
 		public override void Set(string name, float value) { targetMaterial.SetFloat(name, value); }
 		public override void Set(string name, Matrix4x4 value) { targetMaterial.SetMatrix(name, value); }
 		public override void Set(string name, Texture value) { targetMaterial.SetTexture(name, value); }
