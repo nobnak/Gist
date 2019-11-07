20548285078d054d9bf77012e77a3a32fb954a58
Nakata Nobuyuki <nobu.nakata@gmail.com>
2019-11-08 07:23:59 +0900

    CustomAttribute の エクステンションを追加

---
+++ Extensions/CustomAttributeExtension.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+namespace nobnak.Gist.Extensions.CustomAttrExt {
+
+    public static class CustomAttributeExtension {
+        
+        public static bool TryGetAttribute<T>(this MemberInfo info, out T attr, bool inherit = true)
+            where T : System.Attribute {
+            attr = info.GetCustomAttribute<T>(inherit);
+            return attr != null;
+        }
+    }
+}
