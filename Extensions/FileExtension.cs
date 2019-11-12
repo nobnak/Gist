ac6ae3c1a27d56703dd4e32d4495b26598c8895b
Nakata Nobuyuki <nobu.nakata@gmail.com>
2019-11-12 19:29:47 +0900

    最後の例外を保存しておく

--- Extensions/FileExtension.cs
+++ Extensions/FileExtension.cs
@@ -7,11 +7,14 @@ namespace nobnak.Gist.Extensions.FileExt {
 
 	public static class FileExtension {
 
+		public static System.Exception lastErrot = null;
+
 		public static bool TrySave(this string path, string text) {
 			try {
                 path.Save(text);
 				return true;
 			} catch (System.Exception e) {
+				lastErrot = e;
                 Debug.Log($"Failed to save file : path={path}, readon={e.Message}");
 				return false;
 			}
@@ -21,7 +24,8 @@ namespace nobnak.Gist.Extensions.FileExt {
                 text = path.Load();
 				return true;
 			} catch (System.Exception e) {
-                Debug.Log($"Faild to Load file : path={path}, readon={e.Message}");
+				lastErrot = e;
+				Debug.Log($"Faild to Load file : path={path}, readon={e.Message}");
 				text = default(string);
 				return false;
 			}
