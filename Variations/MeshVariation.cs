958e6d336eb6df2d43aea3fc180773ddfa8f88a6
Nakata Nobuyuki <nobu.nakata@gmail.com>
2019-11-18 18:31:49 +0900

    同じマテリアルに、複数のメッシュが選べる場合に使う

---
+++ Variations/MeshVariation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace nobnak.Gist.Variations {
+
+    [ExecuteAlways]
+    public class MeshVariation : MonoBehaviour {
+
+        [SerializeField]
+        protected MeshInfo[] infos = new MeshInfo[0];
+
+        #region unity
+        private void OnEnable() {
+            var filter = GetComponent<MeshFilter>();
+
+            if (infos.Length > 0 && filter != null) {
+                var picked = infos[Random.Range(0, infos.Length)];
+                filter.sharedMesh = picked.mesh;
+            }
+        }
+        private void OnDisable() {
+            
+        }
+        #endregion
+
+        [System.Serializable]
+        public class MeshInfo {
+            public Mesh mesh;
+        }
+    }
+}
