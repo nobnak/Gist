4aa362ee7aceb8b146d0307b886a45414e090100
Nakata Nobuyuki <nobu.nakata@gmail.com>
2019-12-20 17:52:00 +0900

    Curve 上にデータを格納するクラスを作成

---
+++ Extensions/IListExtension.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace nobnak.Gist.Extensions.IListExt {
+
+    public static class IListExtension {
+        public static int Bisect<T>(this IList<T> list, T value, IComparer<T> comparer = null) {
+            comparer = comparer ?? Comparer<T>.Default;
+
+            int lower = 0;
+            int upper = list.Count - 1;
+
+            while (lower <= upper) {
+                int middle = lower + (upper - lower) / 2;
+                int comparisonResult = comparer.Compare(value, list[middle]);
+                if (comparisonResult == 0)
+                    return middle;
+                else if (comparisonResult < 0)
+                    upper = middle - 1;
+                else
+                    lower = middle + 1;
+            }
+
+            return ~lower;
+        }
+    }
+}
--- MathAlgorithms/Curves/Common/DatabaseOnCurve.cs
+++ MathAlgorithms/Curves/Common/DatabaseOnCurve.cs
@@ -1,19 +1,73 @@
-﻿using System.Collections;
+﻿using nobnak.Gist.Extensions.IListExt;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace nobnak.Gist.MathAlgorithms.Curves.Common {
 
+    public class DatabaseOnCurve<T> : IEnumerable<DatabaseOnCurve<T>.Row> {
 
-    public class DatabaseOnCurve : MonoBehaviour {
-        // Start is called before the first frame update
-        void Start() {
+        protected SortedList<float, Row> database = new SortedList<float, Row>();
 
+        public DatabaseOnCurve(BaseCurve curve) {
+            this.Curve = curve;
         }
 
-        // Update is called once per frame
-        void Update() {
-
+        #region interface
+        #region IEnumerable
+        public IEnumerator<Row> GetEnumerator() {
+            return database.Values.GetEnumerator();
         }
+        IEnumerator IEnumerable.GetEnumerator() {
+            return GetEnumerator();
+        }
+        #endregion
+
+        public BaseCurve Curve { get; protected set; }
+
+        public int IndexOf(float t) {
+            var i = database.Keys.Bisect(t);
+            if (i < 0)
+                i = ~i;
+            return i;
+        }
+        public IEnumerable<Row> GetEnumeratorFrom(float t) {
+            for (var i = IndexOf(t); i < database.Count; i++)
+                yield return database[i];
+        }
+        public Row AddAt(float t, T value) {
+            t = Mathf.Clamp(t, 0f, Curve.ParameterLength);
+            var r = new Row(Curve, t, value);
+            database.Add(t, r);
+            return r;
+        }
+        public bool Remove(Row r) {
+            var i = database.IndexOfValue(r);
+            var found = (i >= 0);
+            if (found)
+                database.RemoveAt(i);
+            return found;
+        }
+        #endregion
+
+        #region static
+        public static implicit operator BaseCurve(DatabaseOnCurve<T> me) {
+            return me.Curve;
+        }
+        #endregion
+
+        #region classes
+        public struct Row {
+            public readonly BaseCurve curve;
+            public readonly float t;
+            public readonly T value;
+
+            public Row(BaseCurve curve, float t, T value) {
+                this.curve = curve;
+                this.t = t;
+                this.value = value;
+            }
+        }
+        #endregion
     }
 }
