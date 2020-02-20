eaa59d366e1f713f1dc740b34d0ac5de30efef60
Nakata Nobuyuki <nobu.nakata@gmail.com>
2020-02-20 19:37:33 +0900

    オブジェクトのスワップ関数を追加

---
+++ Util/Swap.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace nobnak.Gist.Util {
+
+    public static class Swap {
+
+        public static void Do<T>(ref T a, ref T b) {
+            var tmp = a;
+            a = b;
+            b = tmp;
+        }
+    }
+}
