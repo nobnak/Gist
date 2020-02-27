64d56fea75a807da62531931abed9ae6133598a6
Nakata Nobuyuki <nobu.nakata@gmail.com>
2020-02-27 14:19:31 +0900

    サブ領域に Blit する機能を追加

---
+++ GLTools/Copy.cs
@@ -0,0 +1,44 @@
+﻿using nobnak.Gist.ObjectExt;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace nobnak.Gist.GLTools {
+    public class Copy : System.IDisposable {
+
+        public enum Pass { Default = 0 }
+
+        public const string PATH = "Copy";
+        public static readonly int P_LocalMat = Shader.PropertyToID("_LocalMat");
+
+        protected Material mat;
+
+        public Copy() {
+            mat = new Material(Resources.Load<Shader>(PATH));
+        }
+
+        #region interface
+        #region IDisposable
+        public void Dispose() {
+            if (mat != null) {
+                mat.DestroySelf();
+                mat = null;
+            }
+        }
+        #endregion
+
+        public void Blit(Texture src, RenderTexture dst,
+            float width = 1f,
+            float height = 1f,
+            float woffset = 0f,
+            float hoffset = 0f
+            ) {
+            var localMat = new Vector4(width, height, woffset, hoffset);
+            mat.SetVector(P_LocalMat, localMat);
+
+            Graphics.Blit(src, dst, mat, (int)Pass.Default);
+        }
+
+        #endregion
+    }
+}
---
+++ GLTools/Resources/Copy.shader
@@ -0,0 +1,47 @@
+﻿Shader "Hidden/Copy" {
+    Properties {
+        _MainTex ("Texture", 2D) = "white" {}
+    }
+    SubShader {
+        Cull Off ZWrite Off ZTest Always
+
+		CGINCLUDE
+			#include "UnityCG.cginc"
+
+			struct appdata {
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f {
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			sampler2D _MainTex;
+			float4 _LocalMat;
+
+			v2f vert(appdata v) {
+				float3 vertex = v.vertex;
+				vertex.xy = vertex.xy * _LocalMat.xy + _LocalMat.zw;
+
+				v2f o;
+				o.vertex = UnityObjectToClipPos(vertex);
+				o.uv = v.uv;
+				return o;
+			}
+		ENDCG
+
+        Pass {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+			float4 frag (v2f IN) : SV_Target {
+				float4 cmain = tex2D(_MainTex, IN.uv);
+				return cmain;
+            }
+            ENDCG
+        }
+    }
+}
