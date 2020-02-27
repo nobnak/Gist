a3fdc84fa15ea762455c81fc22dfed30710e1fd4
Nakata Nobuyuki <nobu.nakata@gmail.com>
2020-02-27 15:22:46 +0900

    Copy.DrawTexture 用のマテリアルを Resources フォルダに用意する

--- Extensions/VisualizationExtension.cs
+++ Extensions/VisualizationExtension.cs
@@ -1,4 +1,5 @@
-﻿using nobnak.Gist.Scoped;
+﻿using nobnak.Gist.GLTools;
+using nobnak.Gist.Scoped;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -20,17 +21,27 @@ namespace nobnak.Gist.Extensions.VisualisationExt {
 
             GL.PushMatrix();
             GL.LoadPixelMatrix();
-
             var height = screen.y * size;
-            var offset = new Vector2(gap, gap + height);
+            var offset = new Vector2(gap, gap);
             foreach (var tex in targets) {
-                var aspect = (float)tex.width / tex.height;
+                var srcWidth = (float)tex.width;
+                var srcHeight = (float)tex.height;
+
+                var aspect = (float)srcWidth / srcHeight;
                 var width = height * aspect;
 
-                var rect = new Rect(offset.x, offset.y, width, -height);
+#if false
                 using (new RenderTextureActivator(destination)) {
+                    var rect = new Rect(offset.x, offset.y, width, -height);
                     Graphics.DrawTexture(rect, tex, mat);
                 }
+#else
+                var outputWidth = size * srcWidth / screen.x;
+                var outputHeight = size * srcHeight / screen.y;
+                var woffset = offset.x / screen.x;
+                var hoffset = offset.y / screen.y;
+                Copy.DrawTexture(tex, destination, outputWidth, outputHeight, woffset, hoffset);
+#endif
 
                 offset.x += gap + width;
             }
--- GLTools/Copy.cs
+++ GLTools/Copy.cs
@@ -4,39 +4,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace nobnak.Gist.GLTools {
-    public class Copy : System.IDisposable {
+    public static class Copy {
 
         public enum Pass { Default = 0 }
 
-        public const string PATH = "Copy";
+        public const string PATH = "Hidden_Copy";
         public static readonly int P_LocalMat = Shader.PropertyToID("_LocalMat");
 
-        protected Material mat;
-
-        public Copy() {
-            mat = new Material(Resources.Load<Shader>(PATH));
-        }
+        public static Material mat;
 
         #region interface
-        #region IDisposable
-        public void Dispose() {
-            if (mat != null) {
-                mat.DestroySelf();
-                mat = null;
+        public static Material LoadedMat {
+            get {
+                if (mat == null)
+                    mat = Resources.Load<Material>(PATH);
+                return mat;
             }
         }
-        #endregion
-
-        public void Blit(Texture src, RenderTexture dst,
+        public static void DrawTexture(Texture src, RenderTexture dst,
             float width = 1f,
             float height = 1f,
             float woffset = 0f,
             float hoffset = 0f
             ) {
             var localMat = new Vector4(width, height, woffset, hoffset);
-            mat.SetVector(P_LocalMat, localMat);
+            LoadedMat.SetVector(P_LocalMat, localMat);
 
-            Graphics.Blit(src, dst, mat, (int)Pass.Default);
+            Graphics.Blit(src, dst, LoadedMat, (int)Pass.Default);
         }
 
         #endregion
