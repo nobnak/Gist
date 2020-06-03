536d4e5a0a236140bee199e5fc2ff253dbe8ad5e
Nakata Nobuyuki <nobu.nakata@gmail.com>
2020-06-03 20:40:42 +0900

    Quaternion を Vector4 に変換する拡張メソッドの名称を明確に

--- Extensions/QuaternionExtension.cs
+++ Extensions/QuaternionExtension.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 namespace nobnak.Gist.Extension.QuaternionExt {
@@ -8,7 +8,7 @@ namespace nobnak.Gist.Extension.QuaternionExt {
 
 		public static readonly Vector4 IDENTITY = new Vector4(0, 0, 0, 1);
 
-		public static Vector4 Convert(this Quaternion q) {
+		public static Vector4 ToVector4(this Quaternion q) {
 			return new Vector4(q.x, q.y, q.z, q.w);
 		}
 
