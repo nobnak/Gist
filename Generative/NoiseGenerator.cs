0f36bf70a743c14ef445aadc66354570bf1985bf
Nakata Nobuyuki <nobu.nakata@gmail.com>
2020-07-28 18:51:14 +0900

    NoiseGenerator にデフォルトコンストラクタがないため、reflection でエラーが出ていた

--- Generative/NoiseGenerator.cs
+++ Generative/NoiseGenerator.cs
@@ -10,17 +10,32 @@ namespace nobnak.Gist.Generative {
         [Range(0f, 2f)]
         public float gain = 1f;
 
-        public NoiseGenerator(float gain) {
+		#region constructors
+		public NoiseGenerator(float gain) {
             this.gain = gain;
         }
+		public NoiseGenerator() : this(1f) { }
+		public static implicit operator NoiseGenerator(float gain) {
+			return new NoiseGenerator(gain);
+		}
+		#endregion
 
-        public float Noise(float x, float y, float t) {
-            return Noise (x, y, t, 1f, 1f);
+		public float Noise(float x, float y, float t) {
+            return _Noise (gain, x, y, t, 1f, 1f);
         }
         public float Noise(float x, float y, float t, float globalGain, float globalExpo) {
-            var v = (globalGain * gain) * (float)SimplexNoise.Noise (x, y, t);
-            return v;
+			return _Noise(gain, x, y, t, globalGain, globalExpo);
         }
-    }
+
+		#region static
+		public static float _Noise(float gain, float x, float y, float t) {
+			return _Noise(gain, x, y, t, 1f, 1f);
+		}
+		public static float _Noise(float gain, float x, float y, float t, float globalGain, float globalExpo) {
+			var v = (globalGain * gain) * (float)SimplexNoise.Noise(x, y, t);
+			return v;
+		}
+		#endregion
+	}
 
 }
