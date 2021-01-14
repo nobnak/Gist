c7875f91088ac9df57dbd715f252d11211b5b0ee
Nakata Nobuyuki <nobu.nakata@gmail.com>
2021-01-14 19:08:48 +0900

    有効数字の計算

---
+++ MathAlgorithms/Util.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace nobnak.Gist.MathAlgorithms {
+
+    public static class Util {
+
+        public static double SignificantFigure(this double value, int digits) {
+            if (digits <= 0)
+                return value;
+
+            var significandGen = (decimal)Math.Pow(10, Math.Floor(Math.Log10(value)) + 1);
+            return (double)(significandGen * Math.Round((decimal)value / significandGen, digits));
+        }
+        public static float SignificantFigure(this float value, int digits) {
+            return (float)SignificantFigure((double)value, digits);
+        }
+    }
+}
