b7da497eb5cb2f0fc2704aebc2bae6b2fc759897
Nakata Nobuyuki <nobu.nakata@gmail.com>
2021-01-18 20:43:52 +0900

    有効数字の修正

--- MathAlgorithms/Util.cs
+++ MathAlgorithms/Util.cs
@@ -8,10 +8,10 @@ namespace nobnak.Gist.MathAlgorithms {
     public static class Util {
 
         public static double SignificantFigure(this double value, int digits) {
-            if (digits <= 0)
-                return value;
+            if (-double.Epsilon <= value && value <= double.Epsilon)
+                return 0;
 
-            var significandGen = (decimal)Math.Pow(10, Math.Floor(Math.Log10(value)) + 1);
+            var significandGen = (decimal)Math.Pow(10, Math.Floor(Math.Log10(Math.Abs(value))) + 1);
             return (double)(significandGen * Math.Round((decimal)value / significandGen, digits));
         }
         public static float SignificantFigure(this float value, int digits) {
