d8b8ea7aa6834062119440b7c20e87dd4da70121
Nakata Nobuyuki <nobu.nakata@gmail.com>
2021-08-24 22:07:38 +0900

    GameObject の enabled 情報をイベント化

---
+++ Events/SwitchOnEnable.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace nobnak.Gist.Events {
+
+    [ExecuteAlways]
+    public class SwitchOnEnable : MonoBehaviour {
+
+        [SerializeField]
+        protected Events events = new Events();
+
+        private void OnEnable() {
+            Notify(true);
+        }
+        private void OnValidate() {
+            Notify(enabled);
+        }
+        private void OnDisable() {
+            Notify(false);
+        }
+
+        private void Notify(bool e) {
+            events.Enabled.Invoke(e);
+            events.Disabled.Invoke(!e);
+        }
+
+        [System.Serializable]
+        public class Events {
+            public BoolEvent Enabled = new BoolEvent();
+            public BoolEvent Disabled = new BoolEvent();
+        }
+    }
+}
